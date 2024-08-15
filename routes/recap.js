5c37b6c3b0621aa21ca0a895cb6dfc46e7e78f1e
Sebastian Pross <spross002@gmail.com>
2024-08-15 13:26:37 -0400

    Fixed a problem where the recap page would crash the website if there wasn't any workouts for the week.

--- routes/recap.js
+++ routes/recap.js
@@ -47,6 +47,11 @@ router.get('/recap', logged_in, async (req, res) => {
     //Now we have the dates we are calculating the recap in, we can query the database for everything within that range
     //Get the workouts for the week
     const week_workouts = await req.db.getAllWorkoutsForWeek(userId, lastMonday, formattedToday);
+    console.log(week_workouts);
+
+    if(week_workouts.length == 0){
+        res.render('recap', { user: user, noWorkouts: true });
+    }
 
     let workout_ids = [];
 
@@ -74,7 +79,7 @@ router.get('/recap', logged_in, async (req, res) => {
     //Take all of the information gathered and pass it into the function to generate the weekly recap
     const recap = calculateWeeklyBreakdown(userId, week_workouts, week_exercises, week_sportActivities, exerciseTable);
 
-    res.render('recap', { user: user, recap: recap })
+    res.render('recap', { user: user, recap: recap, noWorkouts: false });
 })
 
 module.exports = router;
